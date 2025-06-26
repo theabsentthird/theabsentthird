cf9c68870fd3a46746b3b863ae787287c5f757c5
Andrei Barcovschi <abarcovschi@gmail.com>
2025-06-26 17:05:33 +0000

    fixed user schema

--- absent-app/backend/models/User.js
+++ absent-app/backend/models/User.js
@@ -4,6 +4,13 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String, // Hashed password
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  phoneNumber: { type: String, unique: true, sparse: true }, // Optional phone number
+  profilePicture: { type: String, default: '' }, // URL to profile picture
+  bio : { type: String, default: '' }, // User bio
+  role: { type: String, enum: ['user', 'admin', 'business'], default: 'user' }, // User role
+
 });
 
 module.exports = mongoose.model('User', userSchema);
